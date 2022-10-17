871f82e7fbe864627899b1de799f60f619a8e063
Fatemeh Abdi <fatemehabdina@gmail.com>
2022-10-17 17:15:20 +0330

    update

--- Assets/Scripts/WheelSuspension.cs
+++ Assets/Scripts/WheelSuspension.cs
@@ -1,21 +1,22 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class WheelSuspension : MonoBehaviour
 {
     private Rigidbody _rigidbody;
 
+    
+    [Header("Wheel position")]
     public bool wheelFrontLeft;
     public bool wheelFrontRight;
     public bool wheelRearRight;
     public bool wheelRearLeft;
 
-    public bool isOnGround;
-
-    public float tGripFactor;
-
+    
 
     [Header("Suspension")] public float restLength;
     public float springTravel;
@@ -32,28 +33,23 @@ public class WheelSuspension : MonoBehaviour
     private float _springVelocity;
     private Vector3 _suspensionForce;
 
-    private Vector3 _forwardForce;
-    private Vector3 _rightForce;
 
-
-    private float wheelPos;
-
-
-    [Header("Wheel")] public float wheelRadius;
+    [Header("Wheel")] 
+    public float wheelRadius;
     public float steerAngle;
     public float steerTime;
-    public float wheelSpeed;
     public Transform wheelMesh;
-    // [Range(0, 1)] public float tireGripFactor;
-    public float tireMass;
 
+    private float _wheelCurrentAngle;
+
+
+    [Header("About car controlling")]
     public float accelInput;
     public float carTopSpeed;
     public AnimationCurve powerCurve;
     public AnimationCurve gripFactor;
-
-    private float _wheelCurrentAngle;
-    private Vector3 _wheelVelocityLocalSpace;
+    // [Range(0, 1)] public float tireGripFactor;
+    public float wheelGripFactor;
 
     void Start()
     {
@@ -65,88 +61,89 @@ public class WheelSuspension : MonoBehaviour
 
     private void Update()
     {
-        RotateYAxis();
-
-        Debug.DrawRay(transform.position, -transform.up * (_springLength + wheelRadius), Color.magenta);
+        WheelMeshVision();
     }
 
     void FixedUpdate()
     {
         if (Physics.Raycast(transform.position, -transform.up, out RaycastHit hit, _maxLength + wheelRadius))
         {
-            _lastSpringLength = _springLength;
-            _springLength = hit.distance - wheelRadius;
-            _springLength = Mathf.Clamp(_springLength, _minLength, _maxLength);
-
-            _springVelocity = (_lastSpringLength - _springLength) / Time.fixedDeltaTime;
-
-            _springForce = springStiffness * (restLength - _springLength);
-            _dampingForce = dampingStiffness * _springVelocity;
-            _suspensionForce = (_springForce + _dampingForce) * transform.up;
-
-            _wheelVelocityLocalSpace = transform.InverseTransformDirection(_rigidbody.GetPointVelocity(hit.point));
-            _forwardForce = (Input.GetAxis("Vertical") * wheelSpeed) * transform.forward;
-            _rightForce = (_wheelVelocityLocalSpace.x * wheelSpeed) * -transform.right;
-
-
-            // _rigidbody.AddForceAtPosition(_suspensionForce +_forwardForce + _rightForce ,hit.point);
-
-            // _rigidbody.AddForceAtPosition(_suspensionForce ,hit.point);
-            _rigidbody.AddForceAtPosition(_suspensionForce, transform.position);
-
-
-            Vector3 steeringDir = transform.right;
-            Vector3 tireWorldVelocity = _rigidbody.GetPointVelocity(transform.position);
-            float steeringVel = Vector3.Dot(steeringDir, tireWorldVelocity);
-            // tGripFactor = gripFactor.Evaluate(Mathf.Clamp01(steeringVel / tireWorldVelocity.magnitude));
+     
+            Suspension(hit);
             
-            float desiredVelChange = -steeringVel * tGripFactor;
-            float desiredAccel = desiredVelChange / Time.fixedDeltaTime;
-            _rigidbody.AddForceAtPosition(steeringDir* desiredAccel*_rigidbody.mass * 0.05f * 0.5f, transform.position);
-
-
-            Vector3 accelDir = transform.forward;
+            Steering();
             
-                accelInput = (accelInput >= 0f) ? 0.8f : -0.6f;
-                float carSpeed = Vector3.Dot(transform.root.transform.forward, _rigidbody.velocity);
-                float normalizedSpeed = Mathf.Clamp01(Mathf.Abs(carSpeed) / carTopSpeed);
-                float availableTorque = powerCurve.Evaluate(normalizedSpeed) * accelInput;
-            
-                _rigidbody.AddForceAtPosition(accelDir * availableTorque * _rigidbody.mass*10, transform.position);
-                
-                wheelMesh.Rotate(Vector3.right,carSpeed*50 * Time.fixedDeltaTime);
-                // wheelMesh.Rotate(Vector3.right,carSpeed*50 * Time.fixedDeltaTime, Space.Self);
-                // wheelMesh.Rotate(wheelMesh.transform.right,carSpeed*50 * Time.fixedDeltaTime, Space.World);
-
-                // var localRotation = wheelMesh.localRotation;
-                // print(carSpeed * 200 * Time.fixedDeltaTime * Time.time + "+" +  localRotation.eulerAngles.x+ "==" +  (carSpeed * 200 * Time.fixedDeltaTime + localRotation.eulerAngles.x ));
-                // localRotation = Quaternion.Euler(carSpeed * 200 * Time.fixedDeltaTime * Time.time + localRotation.eulerAngles.x , 0,0);
-                // print("?=" + localRotation.eulerAngles.x);
-                //
-                // wheelMesh.localRotation = localRotation;
-                
-
-
-            SetWheelMeshPos();
-            isOnGround = true;
+            Acceleration();
         }
         else
         {
             _springLength = _maxLength;
-            isOnGround = false;
-            print("Fuck");
         }
     }
 
-    private void SetWheelMeshPos()
-    {
-        wheelMesh.localPosition = new Vector3(0, -_springLength, 0);
-        
-    }
-
-    private void RotateYAxis()
+    private void WheelMeshVision()
     {
         _wheelCurrentAngle = Mathf.Lerp(_wheelCurrentAngle, steerAngle, steerTime * Time.deltaTime);
         transform.localRotation = Quaternion.Euler(Vector3.up * _wheelCurrentAngle);
+        
+        wheelMesh.localPosition = new Vector3(0, -_springLength, 0);
+        
+        wheelMesh.Rotate(Vector3.right,Vector3.Dot(transform.root.transform.forward, _rigidbody.velocity)*50 * Time.fixedDeltaTime);
+    }
+    
+
+    public void Suspension(RaycastHit hit)
+    {
+        _lastSpringLength = _springLength;
+        _springLength = hit.distance - wheelRadius;
+        _springLength = Mathf.Clamp(_springLength, _minLength, _maxLength);
+
+        _springVelocity = (_lastSpringLength - _springLength) / Time.fixedDeltaTime;
+
+        _springForce = springStiffness * (restLength - _springLength);
+        _dampingForce = dampingStiffness * _springVelocity;
+        _suspensionForce = (_springForce + _dampingForce) * transform.up;
+
+            
+        _rigidbody.AddForceAtPosition(_suspensionForce, transform.position);
+        
+        
+        DrawArrow.ForDebug(transform.position, _suspensionForce.normalized, Color.cyan);
+    }
+
+    public void Steering()
+    {
+        Vector3 steeringDir = transform.right;
+        Vector3 tireWorldVelocity = _rigidbody.GetPointVelocity(transform.position);
+        float steeringVel = Vector3.Dot(steeringDir, tireWorldVelocity);
+        // tGripFactor = gripFactor.Evaluate(Mathf.Clamp01(steeringVel / tireWorldVelocity.magnitude));
+            
+        float desiredVelChange = -steeringVel * wheelGripFactor;
+        float desiredAccel = desiredVelChange / Time.fixedDeltaTime;
+            
+        Vector3 gripForce = steeringDir* desiredAccel*_rigidbody.mass * 0.05f * 0.1f;
+            
+        _rigidbody.AddForceAtPosition(gripForce, transform.position);
+        
+        
+        DrawArrow.ForDebug(transform.position, gripForce.normalized, Color.yellow);
+    }
+
+    public void Acceleration()
+    {
+        Vector3 accelDir = transform.forward;
+            
+        accelInput = (accelInput >= 0f) ? 0.8f : -0.6f;
+        float carSpeed = Vector3.Dot(transform.root.transform.forward, _rigidbody.velocity);
+        float normalizedSpeed = Mathf.Clamp01(Mathf.Abs(carSpeed) / carTopSpeed);
+        float availableTorque = powerCurve.Evaluate(normalizedSpeed) * accelInput;
+
+        var forwardForce = accelDir * availableTorque * _rigidbody.mass*6;
+        _rigidbody.AddForceAtPosition(forwardForce, transform.position);
+        
+        
+        DrawArrow.ForDebug(transform.position, forwardForce.normalized, Color.green);
+                
+       
     }
 }
