3f8e0415995c1c6f577c5b97c2de7ebd014984e2
Fatemeh Abdi <fatemehabdina@gmail.com>
2022-10-22 23:42:49 +0330

    edit grip values

--- Assets/Scripts/WheelSuspension.cs
+++ Assets/Scripts/WheelSuspension.cs
@@ -9,20 +9,18 @@ public class WheelSuspension : MonoBehaviour
 {
     private Rigidbody _rigidbody;
 
-    
-    [Header("Wheel position")]
-    public bool wheelFrontLeft;
+
+    [Header("Wheel position")] public bool wheelFrontLeft;
     public bool wheelFrontRight;
     public bool wheelRearRight;
     public bool wheelRearLeft;
 
 
-    [Header("Suspension")] 
-    public float restLength;
+    [Header("Suspension")] public float restLength;
     public float springTravel;
     public float springStiffness;
     public float dampingStiffness;
-    
+
     private float _minLength;
     private float _maxLength;
     private float _springLength;
@@ -32,33 +30,25 @@ public class WheelSuspension : MonoBehaviour
     private float _springVelocity;
     private Vector3 _suspensionForce;
 
-    
-    
-    [Header("Forward movement")] 
-    public float accelMultiplier;
+
+    [Header("Forward movement")] public float accelMultiplier;
     public AnimationCurve powerCurve;
     public float accelInput;
     public float carMaxSpeed;
-    
-    
-    [Header("Steering")]
-    public float steerTime;
+
+
+    [Header("Steering")] public float steerTime;
     public float steerAngle;
     public float wheelGripFactor;
     public float wheelGripMultiplier;
     private float _wheelCurrentAngle;
 
-    
 
-
-    [Header("Wheel")] 
-    public float wheelRadius;
+    [Header("Wheel")] public float wheelRadius;
     public Transform wheelMesh;
 
 
-
-    [Header("About car controlling")]
-    public AnimationCurve gripFactor;
+    [Header("About car controlling")] public AnimationCurve gripFactor;
     // [Range(0, 1)] public float tireGripFactor;
 
     void Start()
@@ -78,11 +68,10 @@ public class WheelSuspension : MonoBehaviour
     {
         if (Physics.Raycast(transform.position, -transform.up, out RaycastHit hit, _maxLength + wheelRadius))
         {
-     
             Suspension(hit);
-            
+
             Steering();
-            
+
             Acceleration();
         }
         else
@@ -95,12 +84,13 @@ public class WheelSuspension : MonoBehaviour
     {
         _wheelCurrentAngle = Mathf.Lerp(_wheelCurrentAngle, steerAngle, steerTime * Time.deltaTime);
         transform.localRotation = Quaternion.Euler(Vector3.up * _wheelCurrentAngle);
-        
+
         wheelMesh.localPosition = new Vector3(0, -_springLength, 0);
-        
-        wheelMesh.Rotate(Vector3.right,Vector3.Dot(transform.root.transform.forward, _rigidbody.velocity)*50 * Time.fixedDeltaTime);
+
+        wheelMesh.Rotate(Vector3.right,
+            Vector3.Dot(transform.root.transform.forward, _rigidbody.velocity) * 50 * Time.fixedDeltaTime);
     }
-    
+
 
     public void Suspension(RaycastHit hit)
     {
@@ -114,49 +104,49 @@ public class WheelSuspension : MonoBehaviour
         _dampingForce = dampingStiffness * _springVelocity;
         _suspensionForce = (_springForce + _dampingForce) * transform.up;
 
-            
+
         _rigidbody.AddForceAtPosition(_suspensionForce, transform.position);
-        
-        
+
+
         DrawArrow.ForDebug(transform.position, _suspensionForce.normalized, Color.cyan);
     }
 
     public void Steering()
     {
         Vector3 steeringDir = transform.right;
-        
+
         Vector3 tireWorldVelocity = _rigidbody.GetPointVelocity(transform.position);
         float steeringVel = Vector3.Dot(steeringDir, tireWorldVelocity);
-        
+
         float tGripFactor = gripFactor.Evaluate(Mathf.Clamp01(steeringVel / tireWorldVelocity.magnitude));
-            
+
+        //wheel grip multiplier 0.021 is good enough for now
+        wheelGripMultiplier = 0.021f * (10f / carMaxSpeed);
         float desiredVelChange = -steeringVel * wheelGripFactor;
         float desiredAccel = desiredVelChange / Time.fixedDeltaTime;
-            
-        Vector3 gripForce = steeringDir* desiredAccel*_rigidbody.mass * wheelGripMultiplier;
-            
+
+        Vector3 gripForce = steeringDir * desiredAccel * _rigidbody.mass * wheelGripMultiplier;
+
         _rigidbody.AddForceAtPosition(gripForce, transform.position);
-        
-        
+
+
         DrawArrow.ForDebug(transform.position, gripForce.normalized, Color.yellow);
     }
 
     public void Acceleration()
     {
         Vector3 accelDir = transform.forward;
-            
+
         accelInput = (accelInput >= 0f) ? 1f : -0.8f;
         float carSpeed = Vector3.Dot(transform.root.transform.forward, _rigidbody.velocity);
         float normalizedSpeed = Mathf.Clamp01(Mathf.Abs(carSpeed) / carMaxSpeed);
         float availableTorque = powerCurve.Evaluate(normalizedSpeed) * accelInput;
 
         var forwardForce = accelDir * availableTorque * _rigidbody.mass * accelMultiplier;
-        
+
         _rigidbody.AddForceAtPosition(forwardForce, transform.position);
-        
-        
+
+
         DrawArrow.ForDebug(transform.position, forwardForce.normalized, Color.green);
-                
-       
     }
 }
