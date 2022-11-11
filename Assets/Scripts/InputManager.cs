89fa87c1cd0daca643b002af7f740be209a9eefc
Fatemeh Abdi <fatemehabdina@gmail.com>
2022-11-11 19:23:36 +0330

    changed some variable

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -7,6 +7,9 @@ public class InputManager : MonoBehaviour
     private float _verticalInput;
     private float _horizontalInput;
 
+    public bool touch;
+
+
 
     void Update()
     {
@@ -17,8 +20,35 @@ public class InputManager : MonoBehaviour
 
     private void CheckInput()
     {
-        _horizontalInput = Input.GetAxis("Horizontal");
-        _verticalInput = Input.GetAxis("Vertical");
+        if (touch == true)
+        {
+            foreach (var touch in Input.touches)
+            {
+                
+                if (Input.touchCount == 1 )
+                {
+                    if (touch.position.x > Screen.width - Screen.width / 3)
+                        _horizontalInput = 1;
+                    else if (touch.position.x < Screen.width*1.0f / 3)
+                        _horizontalInput = -1;
+                    _verticalInput = 0;
+                }else if (Input.touchCount == 2 )
+                {
+                    _horizontalInput = 0;
+                    _verticalInput = -1;
+                }
+                else
+                {
+                    _horizontalInput = 0;
+                    _verticalInput = 0;
+                }
+            }
+        }
+        else
+        {
+            _horizontalInput = Input.GetAxis("Horizontal");
+            _verticalInput = Input.GetAxis("Vertical");
+        }
     }
 
     private void UpdateCarControllerInputs()
